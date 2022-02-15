717542664efb9b7631c71c19874232ffababa45b
Mamadou Diagne <genova@dofbi.com>
2022-02-15 08:44:40 +0000

    ♻️ refactor(Page): generator

--- packages/blog/gatsby-blog-core/src/templates/Page.contact.js
+++ packages/blog/gatsby-blog-core/src/templates/Page.contact.js
@@ -1,7 +1,20 @@
 import { graphql } from 'gatsby'
+import { IntlContextConsumer } from "gatsby-plugin-react-intl"
 import PageContact from '../containers/Page.Contact'
+import NotFound from '../Pages/404'
 
-export default PageContact
+export default ({ data, ...props }) => {
+
+  const langue = data.page.nodes[0].language.slug
+
+  return (
+    <IntlContextConsumer>
+      {({ language: currentLocale }) =>
+        currentLocale === langue ? <PageContact {...props} data={data} /> : <NotFound {...props} />
+      }
+    </IntlContextConsumer>
+  )
+}
 
 export const pageQC = graphql`
   query PostPageQC(
