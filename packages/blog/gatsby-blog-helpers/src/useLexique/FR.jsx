f803a688a2f815ff6565a899a1b0ece7da1b5686
Mamadou Diagne <genova@dofbi.com>
2022-04-07 16:20:42 +0000

    ✨ feat(Home): Lexique

---
+++ packages/blog/gatsby-blog-helpers/src/useLexique/FR.jsx
@@ -0,0 +1,45 @@
+import { useStaticQuery, graphql } from 'gatsby'
+
+const useLastFR = () => {
+
+  const { allWpLexique } = useStaticQuery(lexiqueFRQuery)
+  
+  return allWpLexique
+}
+
+const lexiqueFRQuery = graphql`
+  query lexiqueFRQuery {
+    allWpLexique(
+    filter: {status: {eq: "publish"}, language: {code: {eq: FR}}}
+    sort: {fields: [date], order: DESC}
+    limit: 7
+  ) {
+    nodes {
+      id
+      slug
+      title
+      date
+      featuredImage {
+        node {
+          localFile {
+            childImageSharp {
+              __typename
+              ... on ImageSharp {
+                ImageSharp_vertical: gatsbyImageData(
+                  width: 380
+                  height: 290
+                  transformOptions: {cropFocus: CENTER}
+                  outputPixelDensities: [0.5, 1]
+                  quality: 70
+                )
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  }
+`
+
+export default useLastFR
