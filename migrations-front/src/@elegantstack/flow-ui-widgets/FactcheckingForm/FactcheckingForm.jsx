78e3419d4a7d0f3f51497193c5111c8ffa06e83d
Mamadou Diagne <genova@dofbi.com>
2022-04-25 09:33:23 +0000

    ✨ feat(Form): #19 #26 #25 #34

---
+++ migrations-front/src/@elegantstack/flow-ui-widgets/FactcheckingForm/FactcheckingForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card } from 'theme-ui'
+import FactcheckingFormComponent from '@components/FactcheckingForm'
+
+const FactcheckingForm = () => {
+
+  return (
+    <Card variant='paper'>
+      <FactcheckingFormComponent />
+    </Card>
+  )
+}
+
+export default FactcheckingForm
