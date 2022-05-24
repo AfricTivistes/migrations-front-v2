94a6f3bcddcf812ba81accff541389295d9f4414
Mamadou Diagne <genova@dofbi.com>
2022-05-24 07:13:49 +0000

    ♻️ refactor(Algolia): DM-7 Querie #40 #60

---
+++ migrations-front/src/@elegantstack/flow-ui-widgets/Search/Search.Input.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useIntl } from "gatsby-plugin-react-intl"
+import { Box, Input, IconButton } from 'theme-ui'
+import { FaSearch } from 'react-icons/fa'
+import styles from './Search.styles'
+
+const customStyles = {
+  notLoaded: {
+    cursor: `pointer`,
+    color: `transparent`
+  }
+}
+
+const SearchInput = ({ isLoaded, focus, loadSearch, ...props }) => {
+
+  const intl = useIntl()
+
+  return(
+    <>
+      <IconButton
+        sx={styles.mobileTrigger}
+        onClick={loadSearch || props.onFocus}
+        aria-label='Search'
+      >
+        <FaSearch />
+      </IconButton>
+      <Box sx={styles.form({ focus })}>
+        <FaSearch style={styles.searchIcon} />
+        <Input
+          css={!isLoaded && customStyles.notLoaded}
+          sx={styles.input}
+          type='text'
+          placeholder={intl.formatMessage({ id: 'searchPlaceholder' })}
+          aria-label='Search'
+          onClick={loadSearch ? loadSearch : undefined}
+          autoFocus={isLoaded ? true : undefined}
+          {...props}
+        />
+      </Box>
+    </>
+  )
+}
+
+export default SearchInput
