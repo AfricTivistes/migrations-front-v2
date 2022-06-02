d5e45a332505fd9f0abcdab8f4f839c6f83b1593
Mamadou Diagne <genova@dofbi.com>
2022-06-02 04:05:08 +0000

    ♻️ refactor(Ressources): change page template

--- migrations-front/src/@elegantstack/gatsby-blog-core/containers/Ressources.jsx
+++ migrations-front/src/@elegantstack/gatsby-blog-core/containers/Ressources.jsx
@@ -1,61 +1,37 @@
 import React from 'react'
 import { Card as CardComponent } from 'theme-ui'
-import { Layout, Stack, Main, Sidebar, Hero } from '@layout'
-// import CardList from '@components/CardList'
+import { Layout, Stack, Main } from '@layout'
 import Card from '@components/Card'
 import Divider from '@components/Divider'
-import Sticky from '@components/Sticky'
 import Seo from '@widgets/Seo'
-// import TableOfContentsCompact from '@widgets/TableOfContentsCompact'
 import {
   ArticleBody,
-  ArticleFooter
 } from '@widgets/Post'
 
 const Post = ({
-  data: { post, tagCategoryPosts, tagPosts, categoryPosts, previous, next },
+  data: { post },
   ...props
 }) => {
-  
+
   const { pageContext: { services = {}, siteUrl } = {} } = props
-  
+
   return (
     <Layout {...props}>
       <Seo {...post.nodes[0]} siteUrl={siteUrl} />
-      <Hero full>
-        <Card {...post.nodes[0]} variant='horizontal-cover-hero' omitExcerpt />
-      </Hero>
       <Divider />
+      <Stack effectProps={{ effect: 'fadeInDown' }}>
+        <Main>
+          <Card {...post.nodes[0]} variant='horizontal-hero' omitExcerpt />
+        </Main>
+      </Stack>
+      <Divider space={3} />
       <Stack effectProps={{ fraction: 0 }}>
         <Main>
-          <CardComponent variant='paper'>
+          <CardComponent variant='paper-lg'>
             <ArticleBody {...post.nodes[0]} />
-            <ArticleFooter previous={previous.nodes[0]} next={next.nodes[0]} />
           </CardComponent>
-        </Main>
-        <Sidebar>
           <Divider />
-          <Sticky>
-            {/* {post.tableOfContents?.items && (
-              <>
-                <TableOfContentsCompact {...post} />
-                <Divider />
-              </>
-            )}
-            {post.category && (
-              <CardList
-                title='Related Posts'
-                nodes={relatedPosts}
-                variant='horizontal-aside'
-                limit={6}
-                omitMedia
-                omitCategory
-                distinct
-                aside
-              />
-            )} */}
-          </Sticky>
-        </Sidebar>
+        </Main>
       </Stack>
     </Layout>
   )
