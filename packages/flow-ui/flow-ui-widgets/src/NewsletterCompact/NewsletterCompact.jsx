4c3325de77e1761e2ab2df330128531ed1a77bec
mbayedione10 <mbayedione10@gmail.com>
2023-05-08 11:25:02 +0000

    ♻️ refactor (Accueil): Newsletter Text

--- packages/flow-ui/flow-ui-widgets/src/NewsletterCompact/NewsletterCompact.jsx
+++ packages/flow-ui/flow-ui-widgets/src/NewsletterCompact/NewsletterCompact.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import { Card, Text } from 'theme-ui'
 import NewsletterForm from '@components/NewsletterForm'
 import Section from '@components/Section'
+<<<<<<< HEAD
+import { useIntl, FormattedMessage } from 'gatsby-plugin-react-intl'
+
+const NewsletterCompact = props => {
+const intl = useIntl() 
+=======
 import { FormattedMessage } from 'gatsby-plugin-react-intl'
 
 const NewsletterCompact = props => {
 
+>>>>>>> main
 
   return (
-    <Section aside title='Newsletter' {...props}>
+    <Section aside title={intl.formatMessage({ id: "newsletter" })} {...props}> 
       <Card variant='paper'>
         <Text variant='p'>
           <FormattedMessage id='newsletterText'/>
