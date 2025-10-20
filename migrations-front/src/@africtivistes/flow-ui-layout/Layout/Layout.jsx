476c8937fb3284972c9ae1cca7ed18c5676fd374
Abdoulaye Guene <36207283+Abdoulayeguene@users.noreply.github.com>
2025-10-20 15:44:15 +0000

    feat: Mise à jour du chatbot Botpress vers la version v3.3
    - Remplacement du script webchat/v2/inject.js par webchat/v3.3/inject.js
    - Mise à jour du script de configuration vers la version du 15/10/2025
    - Ajout de l'attribut defer au script de configuration
    - Suppression complète des anciens scripts v2
    - Vérification complète du fonctionnement du nouveau chatbot

--- migrations-front/src/@africtivistes/flow-ui-layout/Layout/Layout.jsx
+++ migrations-front/src/@africtivistes/flow-ui-layout/Layout/Layout.jsx
@@ -17,13 +17,13 @@ export const Layout = ({ children, pageContext, location }) => {
   useEffect(() => {
     // Charger le script du chatbot
     const script1 = document.createElement('script');
-    script1.src = 'https://cdn.botpress.cloud/webchat/v2/inject.js';
+    script1.src = 'https://cdn.botpress.cloud/webchat/v3.3/inject.js';
     script1.async = true;
     document.body.appendChild(script1);
 
     const script2 = document.createElement('script');
-    script2.src = 'https://files.bpcontent.cloud/2025/03/24/09/20250324095529-XWB4QYTU.js';
-    script2.async = true;
+    script2.src = 'https://files.bpcontent.cloud/2025/10/15/15/20251015151538-WNYHXSK8.js';
+    script2.defer = true;
     document.body.appendChild(script2);
 
     // Nettoyage lors du démontage du composant
